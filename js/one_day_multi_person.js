34417fead7dc70e7b255d4c86f6333064043c679
ysw <yinshaowen241@gmail.com>
2016-05-27 16:01:15 +0800

    enhacne: one day multi person

--- js/one_day_multi_person.js
+++ js/one_day_multi_person.js
@@ -2,27 +2,28 @@ import $ from "jquery";
 import d3 from 'd3';
 // import * as timepicker from './jquery.timepicker'
 import {get_width, get_height} from './consts';
-import {oneDayMultiPersonData} from './data';
-import {getEventColor} from './utils'
+import {access_data} from './data_access';
+import {getEventColor, str_to_date} from './utils';
 
 
 class Axis {
     constructor(bodyClass) {
-        this.bodyClass = bodyClass;
-        this.wholeDay = [new Date(2016, 1, 1), new Date(2016, 1, 2)];
+      this.bodyClass = bodyClass;
+      this.wholeDay = [new Date(2016, 5, 11), new Date(2016, 5, 12)];
 
-        this.xWidth = 960;
+      this.xWidth = 960;
 
-        this.xScale = d3.time.scale()
-          .domain(this.wholeDay)
-          .range([0, this.xWidth]);
+      this.xScale = d3.time.scale()
+        .domain(this.wholeDay)
+        .range([0, this.xWidth]);
 
-        this._yScale = null;
+      this._yScale = null;
 
-        this.xAxis = d3.svg.axis()
-          .scale(this.xScale)
-          .ticks(d3.time.hour, 3)
-          .orient('bottom');
+      this.xAxis = d3.svg.axis()
+        .scale(this.xScale)
+        // .ticks(d3.time.hour, 3)
+        .orient('bottom');
+
     }
 
     yScale(persons) {
@@ -45,9 +46,9 @@ let main = (bodyClass) => {
   let axisMargin = [40, 40, 40, 40];
   let axisPos = {x: axisMargin[3], y: axisMargin[0]};
 
-  let loadData = (data=oneDayMultiPersonData) => {
+  let loadData = (data) => {
     let index = 0;
-    for (let person of oneDayMultiPersonData) {
+    for (let person of data) {
       let groupHeight = 20; 
       let yRectMargin = 5;
 
@@ -55,6 +56,7 @@ let main = (bodyClass) => {
       let x = axisPos.x;
 
       let rectsGroup = SVG.append('g')
+        .datum(person)
         .attr('class', 'events-rect')
         .attr('transform', 'translate({0}, {1})'
               .format(x, y));
@@ -68,16 +70,16 @@ let main = (bodyClass) => {
         .attr('fill', '#444');
 
       rectsGroup.selectAll('.rect-content')
-        .data(person.events)
+        .data(person.records)
         .enter()
         .append('rect')
         .attr('class', 'rect-content')
         .attr('x', (d) => {
-          return axis.xScale(d.timestamp);
+          return axis.xScale(str_to_date(d.timestamp));
         })
         .attr('y', 0)
         .attr('width', () => {
-          return Math.floor(Math.random() * 40);
+          return 30;
         })
         .attr('height', groupHeight)
         .attr('fill', (d) => {
@@ -85,11 +87,11 @@ let main = (bodyClass) => {
         })
         .append('title')
         .text((d) => {
-          return d.detail;
+          return d.asr_text;
         });
 
       rectsGroup.selectAll('.rect-name')
-        .data(oneDayMultiPersonData)
+        .data(data)
         .enter()
         .append('text')
         .attr('class', 'rect-name')
@@ -98,7 +100,7 @@ let main = (bodyClass) => {
         .attr('font-size', '14px')
         .attr('font-weight', 'bold')
         .text(() => {
-          return person.name;
+          return person.did;
         })
         .attr('fill', '#CCC')
         .on('mouseover', (d, i) => {
@@ -117,19 +119,59 @@ let main = (bodyClass) => {
     
   };
 
+  let xAxisZoom = d3.behavior.zoom()
+    .x(axis.xScale)
+    .scaleExtent([1, 400]);
   
   let SVG = d3.select($(bodyClass)[0])
     .append('svg')
     .attr('width', width + axisMargin[1])
-    .attr('height', height);
+    .attr('height', height)
+    .call(
+      xAxisZoom.on('zoom', () => {
+        let _axis = SVG.select('.axis-odmp');
+        _axis.call(axis.xAxis);
+        console.log(d3.event.scale);
+        // if (d3.event.scale > 20){
+        //     axis.xAxis.ticks(d3.time.minutes, 5);
+        // } else if (d3.event.scale > 10){
+        //     axis.xAxis.ticks(d3.time.minutes, 10);
+        // } else if (d3.event.scale > 6){
+        //     axis.xAxis.ticks(d3.time.minutes, 15);
+        // } else if (d3.event.scale > 4){
+        //     axis.xAxis.ticks(d3.time.minutes, 30);
+        // } else if (d3.event.scale > 2){
+        //     axis.xAxis.ticks(d3.time.hour, 1);
+        // } else if (d3.event.scale > 1){
+        //     axis.xAxis.ticks(d3.time.hour, 2);
+        // } else {
+        //     axis.xAxis.ticks(d3.time.hour, 1);
+        // };
+        let rectsGroups = d3.selectAll('.events-rect')[0];
+        for (let rectsGroup of rectsGroups){
+          rectsGroup = d3.select(rectsGroup);
+          let person = rectsGroup.datum();
+          rectsGroup.selectAll('.rect-content')
+            .data(person.records)
+            .attr('x', (d) => {
+              return axis.xScale(str_to_date(d.timestamp));
+            });
+        }
+      })
+    )
 
   SVG.append('g')
     .attr('class', 'axis-odmp')
     .attr('transform', 'translate({0}, {1})'
           .format(axisPos.x, axisPos.y))
     .call(axis.xAxis);
-  
-  loadData();
+
+  let tmp = (data) => {
+    let persons = [data, data, data];
+    loadData(persons);
+  };
+  access_data('010116000240.json', tmp); 
+
   $('.datepick').datepicker({
     dateFormat: "yy-mm-dd",
     minDate: new Date(2016, 1 - 1, 1),
