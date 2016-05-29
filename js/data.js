26d8cad1cea4747a3f75410ba5cca9712faa013d
ysw <yinshaowen241@gmail.com>
2016-05-30 01:24:57 +0800

    image data, hehe

--- js/data.js
+++ js/data.js
@@ -82,5 +82,88 @@ let oneDayPersonData_no2 = {
   ]
 }
 
+let oneDayPersonData_no3 = {
+  'name': 'dorin',
+  'events': [
+    {
+      'timestamp': new Date(2016, 1, 1, 20),
+      'thing': 'whether',
+      'detial': 'whether detail',
+      'size': 5,
+    },
+    { 'timestamp': new Date(2016, 1, 1, 2),
+      'thing': 'music',
+      'detail': 'music detail\n heheda \n nonon',
+      'size': 3,
+    },
+    {
+      'timestamp': new Date(2016, 1, 1, 14),
+      'thing': 'message',
+      'detail': 'message detail',
+      'size': 2,
+    },
+    {
+      'timestamp': new Date(2016, 1, 1, 7),
+      'thing': 'music',
+      'detail': 'music detail',
+      'size': 6,
+    },
+    {
+      'timestamp': new Date(2016, 1, 1, 8, 40),
+      'thing': 'wiki',
+      'detail': 'wiki detail',
+      'size': 7,
+    },
+    {
+      'timestamp': new Date(2016, 1, 1, 11, 40),
+      'thing': 'story',
+      'detail': 'story detail',
+      'size': 1,
+    }
+  ]
+}
 
-export let oneDayMultiPersonData = [oneDayOnePersonData, oneDayPersonData_no2];
+let oneDayPersonData_no4 = {
+  'name': 'lina',
+  'events': [
+    {
+      'timestamp': new Date(2016, 1, 1, 17),
+      'thing': 'whether',
+      'detial': 'whether detail',
+      'size': 5,
+    },
+    { 'timestamp': new Date(2016, 1, 1, 18),
+      'thing': 'music',
+      'detail': 'music detail\n heheda \n nonon',
+      'size': 3,
+    },
+    {
+      'timestamp': new Date(2016, 1, 1, 11),
+      'thing': 'message',
+      'detail': 'message detail',
+      'size': 2,
+    },
+    {
+      'timestamp': new Date(2016, 1, 1, 7),
+      'thing': 'music',
+      'detail': 'music detail',
+      'size': 6,
+    },
+    {
+      'timestamp': new Date(2016, 1, 1, 9, 40),
+      'thing': 'wiki',
+      'detail': 'wiki detail',
+      'size': 7,
+    },
+    {
+      'timestamp': new Date(2016, 1, 1, 10, 40),
+      'thing': 'story',
+      'detail': 'story detail',
+      'size': 1,
+    }
+  ]
+}
+
+
+export let oneDayMultiPersonData = [oneDayOnePersonData, oneDayPersonData_no2,
+                                    oneDayPersonData_no3, oneDayPersonData_no4];
